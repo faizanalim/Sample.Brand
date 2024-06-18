3ed095334bf84089559d71e948723454185f92f9
faizanali22 <muhammadfaizan.wdc@Outlook.com>
2024-06-18 06:35:41 +0500

    Add JWT

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Sample.Brand.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Sample.Brand.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private Users AuthenticationUser(Users user)
+        {
+            Users _user = null;
+            if (user.UserName == "Admin" && user.Password == "12345")
+            {
+                _user = new Users { UserName = "Faizan" };
+
+            }
+            return _user;
+        }
+
+        private string GenerateToken(Users user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentails = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"], _configuration["Jwt:Audience"], null,
+                expires: DateTime.Now.AddMinutes(1),
+                signingCredentials: credentails
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(Users user)
+        {
+            IActionResult response = Unauthorized();
+            var user_ = AuthenticationUser(user);
+
+            if (user_ != null)
+            {
+                var token =GenerateToken(user_);
+                response = Ok(new { token = token });
+            }
+            return response;
+        }
+    }
+}
